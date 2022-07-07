a741fd1ae43355c1a803e04ca65c5bbb42a1d4ee
Brice Veyna <bricev99@gmail.com>
2022-07-06 17:25:35 -0700

    Add to city and weather data functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,23 +2,28 @@
 var cityInputEl = document.getElementById('inputCity');
 var searchBtn = document.getElementById('search-btn');
 
-// Global variables
+// Global location variables, initially created empty
 var city;
 var state;
+var latitude;
+var longitude;
 
 // Initialize search, store city and state variables
-function initSearch () {
+function initSearch (event) {
+
+    // Prevents default behavior of the button
+    event.preventDefault();
 
     // Store raw input (City, ST) in variable, log to verify
     var fullCity = cityInputEl.value.trim();
     console.log(fullCity);
 
     // Reassign city variable to input before comma, log to verify
-    city = fullCity.split(',')[0];
+    city = fullCity.split(', ')[0];
     console.log(city);
 
     // Reassign state variable to input after comma, log to verify
-    state = fullCity.split(',')[1];
+    state = fullCity.split(', ')[1];
     console.log(state);
 
     // Initialize getCityData function
@@ -27,28 +32,40 @@ function initSearch () {
 
 // Get city data
 function getCityData () {
-    var requestUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "," + state + "&appid=d76d8eb1dc0344e4e9577142cd0e98dd";
+    var requestUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "," + state + ",US&appid=d76d8eb1dc0344e4e9577142cd0e98dd";
 
-    fetch(requestUrl)
-        .then(function (response) {
+    fetch(requestUrl).then(function (response) {
             return response.json();
-        })
-        .then(function (data) {
+        }).then(function (data) {
             console.log(data);
+            latitude = data[0].lat;
+            console.log(latitude);
+            longitude = data[0].lon;
+            console.log(longitude);
+            getWeatherData(latitude, longitude);
+        }).catch(function (error) {
+            console.log(error);
         });
 }
 
 // Get weather data
-function getWeatherData () {
+function getWeatherData (lat, lon) {
     var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=current,minutely,hourly,alerts&appid=d76d8eb1dc0344e4e9577142cd0e98dd";
 
-    fetch(requestUrl)
-        .then(function (response) {
+    fetch(requestUrl).then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data);
+            addWeatherData();
+        }).catch(function (error) {
+            console.log(error);
         });
+}
+
+// Add weather data to cards
+function addWeatherData () {
+
 }
 
 // Start initSearch function when search button is clicked
